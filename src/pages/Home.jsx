d1a2675af8618a11b91d7e05510d1dc7a4e402c8
조주희 <carla_cho@naver.com>
2024-04-04 19:27:10 +0900

    쭈히의 감정 일기장

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+import usePageTitle from "../hooks/usePageTitle";
+
+const getMonthlyData = (pivotDate, data) => {
+    const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime();
+
+    const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1, 0, 23, 59, 59).getTime();
+
+    return data.filter((item) => beginTime <= item.createdDate && item.createdDate <= endTime);
+};
+
+const Home = () => {
+    const data = useContext(DiaryStateContext);
+    const [pivotDate, setPivotDate] = useState(new Date());
+    usePageTitle("감정 일기장");
+
+    const monthlyData = getMonthlyData(pivotDate, data);
+
+    const onIncreaseMonth = () => {
+        // 한 달 후로
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+    };
+    const onDecreaseMonth = () => {
+        // 한 달 전으로
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+    };
+    return (
+        <div>
+            <Header
+                title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+                leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+                rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+            />
+            <DiaryList data={monthlyData} />
+        </div>
+    );
+};
+
+export default Home;
